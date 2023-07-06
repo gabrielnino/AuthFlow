10ed365484da697b41a37e86f5bdc51b7be2f39c
gabrielnino <51062110+gabrielnino@users.noreply.github.com>
2023-07-06 16:04:51 -0700

    Create genesys.

--- AuthFlow.Persistence/Data/AuthFlowDbContext.cs
+++ AuthFlow.Persistence/Data/AuthFlowDbContext.cs
@@ -16,30 +16,11 @@ namespace AuthFlow.Persistence.Data
             Database.EnsureCreated();
             if (!Users.Any())
             {
-                var commonPassword = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
-                var users = new List<User>
-                {
-                    new User { Username = "luis.nino", Password = commonPassword, Email = "luis.nino@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    new User { Username = "maria.perez", Password = commonPassword, Email = "maria.perez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    new User { Username = "juan.gomez", Password = commonPassword, Email = "juan.gomez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    new User { Username = "ana.sanchez", Password = commonPassword, Email = "ana.sanchez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    new User { Username = "pedro.rodriguez", Password = commonPassword, Email = "pedro.rodriguez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    new User { Username = "carla.mendez", Password = commonPassword, Email = "carla.mendez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    new User { Username = "pablo.martinez", Password = commonPassword, Email = "pablo.martinez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    new User { Username = "sara.torres", Password = commonPassword, Email = "sara.torres@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    new User { Username = "lucas.fernandez", Password = commonPassword, Email = "lucas.fernandez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    new User { Username = "marina.castro", Password = commonPassword, Email = "marina.castro@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    new User { Username = "diego.cortez", Password = commonPassword, Email = "diego.cortez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    new User { Username = "eva.gonzalez", Password = commonPassword, Email = "eva.gonzalez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    new User { Username = "andres.morales", Password = commonPassword, Email = "andres.morales@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    new User { Username = "irene.gil", Password = commonPassword, Email = "irene.gil@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
-                    // Añade más usuarios aquí...
-                };
-
-                foreach (var user in users)
+                foreach (var user in Genesys.GetUsers())
                 {
                     Users.Add(user);
                 }
+
                 SaveChanges();
             }
         }
---
+++ AuthFlow.Persistence/Data/Genesys.cs
@@ -0,0 +1,30 @@
+﻿using AuthFlow.Domain.Entities;
+
+namespace AuthFlow.Persistence.Data
+{
+    internal class Genesys
+    {
+        public static List<User> GetUsers()
+        {
+            var commonPassword = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
+            var users = new List<User>
+                {
+                    new User { Username = "luis.nino", Password = commonPassword, Email = "luis.nino@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                    new User { Username = "maria.perez", Password = commonPassword, Email = "maria.perez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                    new User { Username = "juan.gomez", Password = commonPassword, Email = "juan.gomez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                    new User { Username = "ana.sanchez", Password = commonPassword, Email = "ana.sanchez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                    new User { Username = "pedro.rodriguez", Password = commonPassword, Email = "pedro.rodriguez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                    new User { Username = "carla.mendez", Password = commonPassword, Email = "carla.mendez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                    new User { Username = "pablo.martinez", Password = commonPassword, Email = "pablo.martinez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                    new User { Username = "sara.torres", Password = commonPassword, Email = "sara.torres@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                    new User { Username = "lucas.fernandez", Password = commonPassword, Email = "lucas.fernandez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                    new User { Username = "marina.castro", Password = commonPassword, Email = "marina.castro@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                    new User { Username = "diego.cortez", Password = commonPassword, Email = "diego.cortez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                    new User { Username = "eva.gonzalez", Password = commonPassword, Email = "eva.gonzalez@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                    new User { Username = "andres.morales", Password = commonPassword, Email = "andres.morales@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                    new User { Username = "irene.gil", Password = commonPassword, Email = "irene.gil@email.com", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, Active = false },
+                };
+            return users;
+        }
+    }
+}
