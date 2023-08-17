e7fd59c52611e527668aa1b2b3dfe83141c0534f
gabrielnino <51062110+gabrielnino@users.noreply.github.com>
2023-08-16 23:31:04 -0700

    refactor

--- AuthFlow.Application/DTOs/InvalidOperationResultException.cs
+++ AuthFlow.Application/DTOs/InvalidOperationResultException.cs
@@ -4,7 +4,10 @@ namespace AuthFlow.Application.DTOs
     // Represents an exception for invalid operation results.
     public class InvalidOperationResultException : Exception
     {
+        /// <summary>
         // Constructor for the exception that accepts an error message.
+        /// </summary>
+        /// <param name="message">The message</param>
         public InvalidOperationResultException(string message)
             : base(message)  // Call the base exception class with the provided message.
         {
